8412de43b6203b925f0f2a50af28bdc6111fb98c
Tristan Giles <tbgiles@uvic.ca>
2018-01-14 14:52:01 -0800

    Playing around with Flask, previous devs used bottle, see what I can do

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ app = Flask(__name__)
 
 @app.route('/static/<path:path>')
 def static(path):
-    return bottle.static_file(path, root='static/')
+    return app.static_file(path, root='static/')
 
 
-@app.pos
-t('/start')
+@app.post('/start')
 def start():
     data = bottle.request.json
     game_id = data['game_id']
@@ -21,15 +20,15 @@ def start():
     board_height = data['height']
 
     head_url = '%s://%s/static/head.png' % (
-        bottle.request.urlparts.scheme,
-        bottle.request.urlparts.netloc
+        app.request.urlparts.scheme,
+        app.request.urlparts.netloc
     )
 
     # TODO: Do things with data
 
     return {
         'color': '#00FF00',
-        'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
+        'taunt': 'I PITY THE FOO'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'battlesnake-python'
     }
@@ -37,18 +36,18 @@ def start():
 
 @bottle.post('/move')
 def move():
-    data = bottle.request.json
+    data = app.request.get_json
 
-    # TODO: Do things with data
+    # TODO: Do things with data, this is LIKELY where alot of the code will be done
     directions = ['up', 'down', 'left', 'right']
 
     return {
         'move': random.choice(directions),
-        'taunt': 'battlesnake-python!'
+        'taunt': 'I PITY THE FOO'
     }
 
 
 # Expose WSGI app (so gunicorn can find it)
-application = bottle.default_app()
+application = Flask(__name__)
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    application.run(False, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
