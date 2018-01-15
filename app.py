74b5a19799da7680ca2e2fc5edf9aa0b587d0d5a
Tristan Giles <tbgiles@uvic.ca>
2018-01-14 16:45:38 -0800

    made a change to interpret requests

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def home():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
@@ -12,6 +12,26 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
+
+@app.route('/start')
+def start_prog():
+    test1 = {
+    "width": 20
+    "height":20
+    "game_id": "blahblahblah"
+    }
+    # TODO get request params
+    game_id = test1.args.get('game_id')
+    height = test1.args.get('height')
+    width = test1.args.get('width')
+    return """
+    <div>{params}</div>
+    """.format(params=height)
+    #{
+    #"color" : #FF0000
+    #"name" : "Daddy"
+    #}
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
