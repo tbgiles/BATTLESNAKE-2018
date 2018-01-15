5aef12710190bbd601af69fe5f75fb81f4050cb3
Tristan Giles <tbgiles@uvic.ca>
2018-01-14 16:54:54 -0800

    I dont know anymore

--- app.py
+++ app.py
@@ -13,24 +13,13 @@ def home():
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
 
-@app.route('/testytesty')
+@app.route('/start')
 def start_prog():
-    test1 = {
-    "width": 20
-    "height":20
-    "game_id": "blahblahblah"
-    }
     # TODO get request params
-    game_id = test1.args.get('game_id')
-    height = test1.args.get('height')
-    width = test1.args.get('width')
+    height = 3
     return """
     <div>{params}</div>
     """.format(params=height)
-    #{
-    #"color" : #FF0000
-    #"name" : "Daddy"
-    #}
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
