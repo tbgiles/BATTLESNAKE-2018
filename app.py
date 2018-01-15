a314897cb7d64ad12c123255cd84e7cbe03f5baf
Tristan Giles <tbgiles@uvic.ca>
2018-01-14 17:17:53 -0800

    test

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
 
-@app.route('/start', methods=['POST'])
+@app.route('/start')
 def start_prog():
     data = request.args
     # TODO get request params
