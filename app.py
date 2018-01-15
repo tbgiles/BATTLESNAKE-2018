ff664f5ddc454e744ed439b29f9eb261b8573f98
Tristan Giles <tbgiles@uvic.ca>
2018-01-14 16:50:24 -0800

    why is this NOT WORKING

--- app.py
+++ app.py
@@ -12,10 +12,25 @@ def home():
 
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
-#    #"color" : #FF0000
+
+@app.route('/testytesty')
+def start_prog():
+    test1 = {
+    "width": 20
+    "height":20
+    "game_id": "blahblahblah"
+    }
+    # TODO get request params
+    game_id = test1.args.get('game_id')
+    height = test1.args.get('height')
+    width = test1.args.get('width')
+    return """
+    <div>{params}</div>
+    """.format(params=height)
+    #{
+    #"color" : #FF0000
     #"name" : "Daddy"
     #}
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
