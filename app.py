de8f7ad38b5cc39d669e739c58eac26f47814e2b
Tristan Giles <tbgiles@uvic.ca>
2018-01-15 23:06:15 -0800

    Okay, so this is FINALLY working somewhat. Random choices but thats ok

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 import os, random
-app = Flask(__name__)
 
 @app.route("/start", methods=["POST"])
 def start():
     #data = request.get_json()
+
     # TODO get request params
     return jsonify(
-        name="Tristan's Snake",
-        color="#FF0000",
-        secondary_color="#00FF00",
-        head_url="http://placecage.com/c/100/100"
+        name = "Tristan's Snake",
+        color = "#FF0000",
+        secondary_color = "#00FF00",
+        head_url = "http://2.bp.blogspot.com/_qAms05FxvSw/TRy3kgEBjWI/AAAAAAAAAYY/xdK5e6w_P4s/s1600/The%2BRoom%2Bwe%2Bare%2Bexpecting%2521%2B.jpg"
+        taunt = "Why, Lisa, why, WHY?!"
     )
 
 
@@ -24,10 +25,10 @@ def move():
 
     return jsonify(
     move = random.choice(directions),
-    taunt = "YOLO"
+    taunt = "You're tearing me apart, Lisa!"
     )
 
 
-
-if __name__ == '__main__':
+app = Flask(__name__)
+if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
