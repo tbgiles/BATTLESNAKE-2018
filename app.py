5145869af18bffae0cb7fda1f987dd0fe4fb6f10
Tristan Giles <tbgiles@uvic.ca>
2018-01-15 21:36:53 -0800

    Just height

--- app.py
+++ app.py
@@ -17,17 +17,9 @@ def home():
 def start_prog():
     # TODO get request params
     height = request.form('height')
-    width = request.form('width')
-    game_id = request.form('game_id')
-    is_json_type = request.is_json
 
 
-    return """
-    <div>ISJSON is {jsonify}</div>
-    <div>WIDTH is {width}</div>
-    <div>HEIGHT: is {height}</div>
-    <div>language is {game_id}</div>
-    """.format(jsonify=is_json_type, width=width, height=height, game_id=game_id)
+    return "<div>{height}</div>".format(height=height)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
