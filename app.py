6eea71371a713c74f220060d69c57c7684dfec54
Tristan Giles <tbgiles@uvic.ca>
2018-01-16 20:28:16 -0800

    Added a dict of known not-filled positions

--- app.py
+++ app.py
@@ -7,22 +7,21 @@ import os, random, math
 
 app = Flask(__name__) #App is now an instance of Flask.
 
-def get_move_letters(x, y, my_snake_x, my_snake_y):
+def get_move_letters(x, y, my_snake_x, my_snake_y, game_grid):
     disp_x = x - my_snake_x
     disp_y = y - my_snake_y
 
-    if abs(disp_x) > abs(disp_y):
-        if disp_x < 0:
-            return "left"
-        else:
-            return "right"
+    #if abs(disp_x) > abs(disp_y):
+    if disp_x < 0 and !game_grid[[my_snake_x - 1, my_snake_y]]:
+        return "left"
+    else if !game_grid[[my_snake_x + 1, my_snake_y]]:
+        return "right"
+    else if disp_y < 0 and !game_grid[[my_snake_x, my_snake_y + 1]]:
+        return "up"
     else:
-        if disp_y < 0:
-            return "up"
-        else:
-            return "down"
+        return "down"
 
-def get_next_move(food, height, width, snakes, dead_snake, my_snake_x, my_snake_y):
+def get_next_move(food, height, width, snakes, dead_snake, my_snake_x, my_snake_y, game_grid):
     min_dist = width + height
     min_x = 0
     min_y = 0
@@ -34,7 +33,7 @@ def get_next_move(food, height, width, snakes, dead_snake, my_snake_x, my_snake_
             min_dist = dist
             min_x = x
             min_y = y
-    return get_move_letters(min_x, min_y, my_snake_x, my_snake_y)
+    return get_move_letters(min_x, min_y, my_snake_x, my_snake_y, game_grid)
 
 @app.route("/start", methods=["POST"])
 def start():
@@ -59,6 +58,7 @@ def start():
 
 @app.route("/move", methods=["POST"])
 def move():
+    game_grid = {}
     data = request.get_json()
     food = data.get("food") #Array
     game_id = data.get("game_id")
@@ -69,16 +69,20 @@ def move():
     width = data.get("width")
     you = data.get("you")
 
+    for x in range(0, width + 1):
+        for y in range(0, height + 1):
+            game_grid([x, y]) = 0
+
     for snake in snakes:
+        for coordinate_pair in snake.get("coords"):
+            game_grid[coordinate_pair] = 1;
         if snake.get("id") == you:
             my_snake_x = snake.get("coords")[0][0]
             my_snake_y = snake.get("coords")[0][1]
-
-    # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
+            #TODO Maybe simplify this to a 1x2 list?
 
     return jsonify(
-    move = get_next_move(food, height, width, snakes, dead_snake, my_snake_x, my_snake_y),
+    move = get_next_move(food, height, width, snakes, dead_snake, my_snake_x, my_snake_y, game_grid), #TODO This is what controls where the snake goes!
     taunt = "You're tearing me apart, Lisa!"
     )
 
