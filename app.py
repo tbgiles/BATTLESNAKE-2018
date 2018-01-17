0567c95254f98d701e1ca3cdbdcbabcb7a920a64
Tristan Giles <tbgiles@uvic.ca>
2018-01-16 21:05:39 -0800

    It sorta works...

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_move_letters(x, y, my_snake_x, my_snake_y, game_grid):
         return "left"
     elif not game_grid[(my_snake_x + 1, my_snake_y)] == 1:
         return "right"
-    elif disp_y < 0 and not game_grid[(my_snake_x, my_snake_y + 1)] == 1:
+    elif disp_y < 0 and not game_grid[(my_snake_x, my_snake_y - 1)] == 1:
         return "up"
     else:
         return "down"
