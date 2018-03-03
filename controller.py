44b683c434c1deab71dde9a44ba9b63eccf7cf68
Tristan Giles <tbgiles@uvic.ca>
2018-03-03 14:27:37 -0800

    got rid of prints

--- controller.py
+++ controller.py
@@ -51,7 +51,7 @@ def grid_setup(food, width, height, snakes, mySnake, mySnakeID):
     grid_options.append(generic_grid)
     grid_options.append(food_grid)
 
-    print('')
+    '''print('')
     for y in range(0, width):
         print('')
         for x in range(0, height):
@@ -60,7 +60,7 @@ def grid_setup(food, width, height, snakes, mySnake, mySnakeID):
             elif (x, y) in food_grid:
                 print('F', end = '')
             else:
-                print('0', end='')
+                print('0', end='')'''
 
     return grid_options
 
