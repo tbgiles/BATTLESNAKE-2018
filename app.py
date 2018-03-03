7830760b48c1a59e5cedbeb289e16c2daa72491d
Tristan Giles <tbgiles@uvic.ca>
2018-03-02 22:03:29 -0800

    Added a response 200 tuple object

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def move():
 
 @app.route("/end", methods=["POST"])
 def end():
-    return true, 200
+    return ('', 200)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
